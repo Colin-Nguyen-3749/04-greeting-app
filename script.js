d450fb4af3ab95f1eb1796720175cc91d60c13d2
Colin James Nguyen <nguyen_c9@denison.edu>
2025-06-24 18:09:09 +0000

    Button now displays welcome message

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+// Declare a variable to store the user's name
 let userName = "Colin";
-console.log("Welcome, " + userName + "!");
+
+// Get the button element from the page using its id
+let button = document.getElementById("welcomeBtn");
+
+// Get the paragraph element where we will show the welcome message
+let message = document.getElementById("welcomeMsg");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+  // When the button is clicked, show the welcome message using userName
+  message.textContent = "Welcome, " + userName + "!";
+});
