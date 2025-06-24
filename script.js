6349d0dcfd9e8cec2be1fec0bbb0b18d6e82e6c4
Colin James Nguyen <nguyen_c9@denison.edu>
2025-06-24 18:10:25 +0000

    Username is now customizable

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Declare a variable to store the user's name
-let userName = "Colin";
+// Get the input field where the user will type their name
+let nameInput = document.getElementById("nameInput");
 
 // Get the button element from the page using its id
 let button = document.getElementById("welcomeBtn");
@@ -9,6 +9,8 @@ let message = document.getElementById("welcomeMsg");
 
 // Add a click event listener to the button
 button.addEventListener("click", function() {
-  // When the button is clicked, show the welcome message using userName
-  message.textContent = "Welcome, " + userName + "!";
+  // Get the value entered by the user
+  let userName = nameInput.value;
+  // Show a fun personalized message using the user's name
+  message.textContent = "Hey " + userName + "! You are awesome!";
 });
